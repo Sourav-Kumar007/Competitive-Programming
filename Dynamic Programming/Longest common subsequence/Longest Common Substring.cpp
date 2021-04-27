5673fd7e85d1b4bf0751387f5f4fc2e147807793
Sourav-Kumar007 <82237341+Sourav-Kumar007@users.noreply.github.com>
2021-04-28 01:31:53 +0600

    Add files via upload

---
+++ Dynamic Programming/Longest common subsequence/Longest Common Substring.cpp
@@ -0,0 +1,86 @@
+// “Success is not final, failure is not fatal: it is the courage to continue that counts.” — Winston Churchill
+
+/**
+      Institution : Hajee Mohammad Danesh Science and Technology University,Dinajpur,Bangladesh (H S T U).
+      Name        : Sourav Roy (Shuvo)
+      Department  : Computer Science and Engineering (Batch - CSE'19)
+      Email       : souravkumarroy813965@gmail.com
+**/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define int long long
+#define all(a) a.begin(),a.end()
+#define pb push_back
+#define mod 1000000007
+int dp[1001][1001],mx;
+
+int LCS(string s,string t,int n,int m)
+{
+    int dp[n+1][m+1],i,j;
+
+    for(i=0; i<=n; i++)
+    {
+        for(j=0; j<=m; j++)
+        {
+            if(i==0 || j==0)
+            {
+                dp[i][j] = 0;
+            }
+        }
+    }
+
+    for(i=1; i<=n; i++)
+    {
+        for(j=1; j<=m; j++)
+        {
+            if(s[i-1]==t[j-1])
+            {
+                dp[i][j] = 1 + dp[i-1][j-1];
+                mx = max(mx,dp[i][j]);
+            }
+            else
+            {
+                dp[i][j] = 0;
+            }
+        }
+    }
+
+
+    return mx;
+}
+
+void solve()
+{
+    int n,m;
+    string s,t;
+
+    cin >> n >> m >> s >> t;
+
+    cout << LCS(s,t,n,m) << endl;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t=1;
+    while(t--)
+    {
+        solve();
+    }
+}
+/**
+Test Case :
+Input :
+5 5
+axxxb
+xxxrt
+
+Output ;
+3
+**/
