5a6960423266eceb667417ada7487b4e66835a1a
Sourav-Kumar007 <82237341+Sourav-Kumar007@users.noreply.github.com>
2021-04-30 12:50:36 +0600

    Add files via upload

---
+++ Dynamic Programming/Longest common subsequence/Longest Palindromic Subsequence.cpp
@@ -0,0 +1,110 @@
+// “Success is not final, failure is not fatal: it is the courage to continue that counts.” — Winston Churchill
+
+/**
+      Institution : Hajee Mohammad Danesh Science and Technology University,Dinajpur,Bangladesh (H S T U).
+      Name        : Sourav Roy (Shuvo)
+      Department  : Computer Science and Engineering (Batch - CSE'19)
+      Email       : souravkumarroy813965@gmail.com
+**/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define int long long
+#define all(a) a.begin(),a.end()
+#define pb push_back
+#define mod 1000000007
+
+void LPS(string s,string t,int n,int m)
+{
+    int dp[n+1][m+1],i,j;
+
+    for(i=0; i<=n; i++)
+    {
+        for(j=0; j<=m; j++)
+        {
+            if(i==0 || j==0)
+            {
+                dp[i][j] = 0;
+            }
+        }
+    }
+
+    for(i=1; i<=n; i++)
+    {
+        for(j=1; j<=m; j++)
+        {
+            if(s[i-1]==t[j-1])
+            {
+                dp[i][j] = 1 + dp[i-1][j-1];
+            }
+            else
+            {
+                dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+    }
+
+    string ans;
+
+    i = n;
+    j = m;
+    while(i && j)
+    {
+        if(s[i-1]==t[j-1])
+        {
+            ans += s[i-1];
+            i--;
+            j--;
+        }
+        else
+        {
+            if(dp[i-1][j]>dp[i][j-1])
+            {
+                i--;
+            }
+            else
+            {
+                j--;
+            }
+        }
+    }
+
+    reverse(all(ans));
+
+    cout << ans << endl;
+}
+
+void solve()
+{
+    string s,t,temp;
+    cin >> s;
+    temp = s;
+    reverse(all(s));
+    LPS(temp,s,s.length(),s.length());
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t=1;
+    while(t--)
+    {
+        solve();
+    }
+}
+/**
+Test Case :
+Input :
+mnaqnpsgt
+Output :
+nqn
+
+Input :
+bgbmnpqr
+Output : bgb
+**/
