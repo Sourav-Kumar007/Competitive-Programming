5bc89716c39edb3dfac44c42c7920015ff3705d4
Sourav-Kumar007 <82237341+Sourav-Kumar007@users.noreply.github.com>
2021-12-16 13:48:30 +0600

    Add files via upload

---
+++ Graph/Counting Connected Components.cpp
@@ -0,0 +1,89 @@
+/*
+ *
+ * FILE: C.cpp
+ *
+ * Institution : Hajee Mohammad Danesh Science and Technology University,Dinajpur,Bangladesh (H S T U).
+ *
+ * Name        : Sourav Roy (Shuvo)
+ *
+ * Department  : Computer Science and Engineering (Batch - CSE'19)
+ *
+ * Email       : souravkumarroy813965@gmail.com
+ *
+ */
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+#define endl '\n'
+#define int long long
+#define all(a) a.begin(),a.end()
+#define pb push_back
+#define mod 1000000007
+vector <int> arr[100001];
+int vis[100001];
+
+void dfs(int node)
+{
+    vis[node] = 1;
+    for(int child : arr[node])
+    {
+        if(!vis[child])
+        {
+            dfs(child);
+        }
+    }
+}
+
+void solve()
+{
+    int n,m,i,a,b,cc=0;
+    cin >> n >> m;
+    for(i=1; i<=m; i++)
+    {
+        cin >> a >> b;
+        arr[a].pb(b);
+        arr[b].pb(a);
+    }
+
+    for(i=1; i<=n; i++)
+    {
+        if(!vis[i])
+        {
+            dfs(i);
+            cc++;
+        }
+    }
+
+    cout << "Total connected components : " << cc << endl;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t=1;
+    while(t--)
+    {
+        solve();
+    }
+}
+/**
+Test Case :
+13 10
+1 2
+1 4
+2 3
+4 5
+4 6
+6 7
+6 8
+9 10
+11 12
+12 13
+**/
