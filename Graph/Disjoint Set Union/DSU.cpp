e8ff3152cd60650d50c99b2bce8abe95e1cdc893
Sourav-Kumar007 <82237341+Sourav-Kumar007@users.noreply.github.com>
2022-01-08 12:07:46 +0600

    Add files via upload

---
+++ Graph/Disjoint Set Union/DSU.cpp
@@ -0,0 +1,91 @@
+///    Success is not a Miracle , it's a journey of DEDICATION   ///
+
+/*
+ *
+ * FILE: C.cpp
+ *
+ * Institution : Hajee Mohammad Danesh Science and Technology University,Dinajpur,Bangladesh (H S T U).
+ *
+ * Name        : Sourav Roy (Shuvo)
+ *
+ * Department  : Computer Science and Engineering (Batch - CSE'19)
+ *
+ * Email       : souravkumarroy813965@gmail.com
+ *
+ */
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+#define endl '\n'
+#define int long long
+#define all(a) a.begin(),a.end()
+#define pb push_back
+#define mod 1000000007
+
+// Graph on 2D Grid
+/*----------------------Graph Moves----------------*/
+//const int dx[]={+1,-1,+0,+0};
+//const int dy[]={+0,+0,+1,-1};
+//const int dx[]={+0,+0,+1,-1,-1,+1,-1,+1};   // Kings Move
+//const int dy[]={-1,+1,+0,+0,+1,+1,-1,-1};  // Kings Move
+//const int dx[]={-2, -2, -1, -1,  1,  1,  2,  2};  // Knights Move
+//const int dy[]={-1,  1, -2,  2, -2,  2, -1,  1}; // Knights Move
+/*------------------------------------------------*/
+int parent[10001];
+
+int finds(int a)
+{
+    if(parent[a]<0)
+    {
+        return a;
+    }
+    return parent[a] = finds(parent[a]);
+}
+
+void uni(int p1,int p2)
+{
+    parent[p1] += parent[p2];
+    parent[p2] = p1;
+}
+
+void solve()
+{
+    int n,i,m,a,b;
+    cin >> n >> m;
+    memset(parent,-1,sizeof(parent));
+    for(i=1; i<=m; i++)
+    {
+        cin >> a >> b;
+        int par1 = finds(a);
+        int par2 = finds(b);
+        if(par1 != par2)
+        {
+            uni(par1,par2);
+        }
+    }
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t=1;
+    while(t--)
+    {
+        solve();
+    }
+}
+/**
+Test Case :
+
+**/
+
+/*"Success isn't permanent, failure isn't fatal,
+                                     it's the courage to continue that counts"*/
+
