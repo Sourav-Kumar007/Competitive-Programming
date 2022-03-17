f4ac4b50f81513ad1ac84f17a948388c9f4bf591
Sourav-Kumar007 <82237341+Sourav-Kumar007@users.noreply.github.com>
2022-03-17 18:24:43 +0600

    Add files via upload

---
+++ Codeforces/E. Rescheduling the Exam.cpp
@@ -0,0 +1,141 @@
+///    Success is not a Miracle , it's a journey of DEDICATION   ///
+
+/*
+ *
+ * FILE: C.cpp
+ *
+ * Institution : Hajee Mohammad Danesh Science and Technology University,Dinajpur,Bangladesh (H S T U).
+ *
+ * Name        : Sourav Roy (Shuvo)
+ *
+ * Department  : Computer Science and Engineering (Batch - CSE'19)
+ *
+ * Email       : souravkumarroy813965@gmail.com
+ *
+ */
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+#define endl '\n'
+#define int long long
+#define all(a) a.begin(),a.end()
+#define pb push_back
+#define mod 1000000007
+
+// Graph on 2D Grid
+/*----------------------Graph Moves----------------*/
+//const int dx[]={+1,-1,+0,+0};
+//const int dy[]={+0,+0,+1,-1};
+//const int dx[]={+0,+0,+1,-1,-1,+1,-1,+1};   // Kings Move
+//const int dy[]={-1,+1,+0,+0,+1,+1,-1,-1};  // Kings Move
+//const int dx[]={-2, -2, -1, -1,  1,  1,  2,  2};  // Knights Move
+//const int dy[]={-1,  1, -2,  2, -2,  2, -1,  1}; // Knights Move
+/*------------------------------------------------*/
+
+bool check(vector <int> v,int x,int d,int n)
+{
+    for(int i=1; i<n; i++)
+    {
+        int dis = v[i] - v[i-1] - 1;
+        if(dis < x)
+        {
+            return false;
+        }
+    }
+    for(int i=1; i<n; i++)
+    {
+        int dis = v[i] - v[i-1] - 1;
+        if(dis > 2 * x)
+        {
+            return true;
+        }
+    }
+    if(d - v.back() - 1 >= x)
+    {
+        return true;
+    }
+    return false;
+}
+
+void solve()
+{
+    int n,d,i,idx,mn=1e18,x,start,end,ans,mid;
+    cin >> n >> d;
+    vector <int> v(n+1,0),temp(n+1,0);
+    for(i=1; i<=n; i++)
+    {
+        cin >> v[i];
+        temp[i] = v[i];
+        x = v[i] - v[i-1] - 1;
+        if(x < mn)
+        {
+            mn = x;
+            idx = i;
+        }
+    }
+    ans = mn;
+
+    v.erase(v.begin()+idx);
+    start = 1;
+    end = 1e9;
+    while(start <= end)
+    {
+        mid = start + (end - start) / 2;
+        if(check(v,mid,d,n))
+        {
+            ans = max(ans, mid);
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+    }
+    idx--;
+    if(idx>0)
+    {
+        temp.erase(temp.begin()+idx);
+        start = 1;
+        end = 1e9;
+        while(start <= end)
+        {
+            mid = start + (end - start) / 2;
+            if(check(temp,mid,d,n))
+            {
+                ans = max(ans, mid);
+                start = mid + 1;
+            }
+            else
+            {
+                end = mid - 1;
+            }
+        }
+    }
+
+    cout << ans << endl;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+}
+/**
+Test Case :
+
+**/
+
+/*"Success isn't permanent, failure isn't fatal,
+                                     it's the courage to continue that counts"*/
