26387e337bc0b914a56d9289d1647f76bde65306
Sourav-Kumar007 <82237341+Sourav-Kumar007@users.noreply.github.com>
2022-05-20 11:45:11 +0600

    Add files via upload

---
+++ Codeforces/template.cpp
@@ -0,0 +1,72 @@
+///    Success is not a Miracle , it's a journey of DEDICATION   ///
+
+/*
+ *
+ * FILE: C.cpp
+ *
+ * Institution : Hajee Mohammad Danesh Science and Technology University,Dinajpur,Bangladesh (H S T U).
+ *
+ * Name        : Sourav Roy (Shuvo)
+ *
+ * Department  : Computer Science and Engineering (Batch - CSE'19)
+ *
+ * Email       : souravkumarroy813965@gmail.com
+ *
+ */
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+#define endl '\n'
+#define int long long
+#define all(a) a.begin(),a.end()
+#define pb push_back
+#define mod 1000000007
+#define inf 1e18+7
+///  order_of_key return number of elements less than x -> os.order_of_key(x)
+///  cout << "oth element  : " << *os.find_by_order(0) << endl; so it returns value of index
+int lcm(int x,int y)
+{
+    return (x * 1LL * y) / __gcd(x,y);
+}
+
+// Graph on 2D Grid
+/*----------------------Graph Moves----------------*/
+//const int dx[]={+1,-1,+0,+0};
+//const int dy[]={+0,+0,+1,-1};
+//const int dx[]={+0,+0,+1,-1,-1,+1,-1,+1};   // Kings Move
+//const int dy[]={-1,+1,+0,+0,+1,+1,-1,-1};  // Kings Move
+//const int dx[]={-2, -2, -1, -1,  1,  1,  2,  2};  // Knights Move
+//const int dy[]={-1,  1, -2,  2, -2,  2, -1,  1}; // Knights Move
+/*------------------------------------------------*/
+
+void solve()
+{
+
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+}
+/**
+Test Case :
+
+**/
+///  stuck in finding min / max problem ? think for binary search
+///  special case : n = 1 ?
+///  try to think in a different way or take a brake from pc and walk away
+/*"Success isn't permanent, failure isn't fatal,
+                                     it's the courage to continue that counts"*/
